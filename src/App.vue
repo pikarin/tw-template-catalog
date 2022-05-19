914ae6bb02be88e4daa9a2fd7cc2681b3c870ce0
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2022-05-19 21:03:54 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+//
+</script>
+
+<template>
+  <nav>
+    <h3>twproject.</h3>
+    <ul>
+      <li>Documentation</li>
+      <li>Github</li>
+    </ul>
+  </nav>
+  <header>
+    <h1>Tailwind based templates.</h1>
+    <h2>Project template develop with tailwindcss.</h2>
+
+    <button>Explore</button>
+  </header>
+
+  <main>
+    <div>
+      <button>All</button>
+      <button>Ecommerce</button>
+      <button>Portfolio</button>
+      <button>Company</button>
+    </div>
+
+    <div>
+      <div>Image1</div>
+      <div>Image2</div>
+      <div>Image3</div>
+      <div>Image4</div>
+    </div>
+
+    <div>
+      <button>View More</button>
+    </div>
+  </main>
+
+  <footer>
+    <div>
+      <h3>twproject.</h3>
+      <p>free template for all.</p>
+    </div>
+
+    <div>
+      <button>Github</button>
+      <button>Documentation</button>
+    </div>
+  </footer>
+</template>
