365921fcb944305682994a1b8cc70b61f1c03460
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2022-05-19 23:12:42 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/theme-catalog/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue()]
 })
